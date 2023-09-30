e786eb42ba35d50c693b7f49a5d339105b3f7f2b
Aman Kumar <75082110+Amank2854@users.noreply.github.com>
2023-10-01 03:41:54 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,74 @@
+# Gita ChatBot using Llama-2-7B-Chat
+
+
+Here's a brief overview of the components of the repository:
+
+- `app.py`: The Streamlit web application code that allows users to interact with the chatbot through a simple user interface.
+
+- `llama-2-7b-chat.ggmlv3.q2_K.bin`:  Quantized model weights provided by TheBloke.   Can be downloaded via 
+
+```
+https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGML/resolve/main/llama-2-7b-chat.ggmlv3.q2_K.bin
+```
+
+- `Dockerfile`: The Dockerfile used to containerize the application for easy deployment and management.
+
+- `docker-compose.yml`: A Docker Compose file that simplifies the deployment of the chatbot with memory as a Docker container.
+
+- `requirements.txt`: Contains a list of Python dependencies required to run the application.
+
+## Prerequisites
+
+Before running the Streamlit Chatbot with Memory, you need to have the following installed:
+
+1. Python
+
+## Installation
+
+To set up the chatbot locally, follow these steps:
+
+1. Clone this repository to your local machine using Git:
+
+```bash
+git clone https://github.com/talhaanwarch/streamlit-llama.git
+cd streamlit-chatbot-memory
+```
+
+2. Create a virtual environment and activate it:
+
+```bash
+python -m venv venv
+venv/Scripts/activate  
+```
+
+3. Install the required Python packages:
+
+```bash
+pip install -r requirements.txt
+```
+
+## Usage
+
+To run the Streamlit Chatbot with Memory, execute the following command:
+
+```bash
+streamlit run app.py
+```
+
+This will start the Streamlit server, and you can access the chatbot interface by opening your web browser and navigating to `http://localhost:8501`.
+
+Simply type your messages in the input box and press "Enter" to send them to the chatbot. The chatbot will respond based on the context of the conversation, thanks to its memory capabilities.
+
+## Docker Compose
+
+If you prefer to deploy the chatbot using Docker Compose, follow these steps:
+
+1. Make sure you have Docker and Docker Compose installed on your system.
+
+2. Start the container using Docker Compose:
+
+```bash
+docker-compose up -d
+```
+
+The chatbot will be accessible at `http://localhost:8501` in your web browser.
---
+++ app.py
@@ -0,0 +1,73 @@
+import streamlit as st
+import os
+from ctransformers import AutoModelForCausalLM
+from langchain.llms import CTransformers
+
+# App title
+st.set_page_config(page_title="Llama 2 Gita Chatbot 🦙💬")
+
+@st.cache_resource()
+def ChatModel(temperature, top_p):
+    return AutoModelForCausalLM.from_pretrained(
+        'llama-2-7b-chat.ggmlv3.q2_K.bin', 
+        model_type='llama',
+        temperature=temperature, 
+        top_p = top_p)
+
+
+with st.sidebar:
+    st.title('Llama 2 Gita Chatbot 🦙💬')
+
+    
+    temperature = 0.5
+    top_p = 0.5
+
+    chat_model =ChatModel(temperature, top_p)
+
+# Store LLM generated responses
+if "messages" not in st.session_state.keys():
+    st.session_state.messages = [{"role": "assistant", "content": "Welcome to Gita ChatBot, How may I assist you today?"}]
+
+# Display or clear chat messages
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+def clear_chat_history():
+    st.session_state.messages = [{"role": "assistant", "content": "Welcome to Gita ChatBot, How may I assist you today?"}]
+st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
+
+# Function for generating LLaMA2 response
+def generate_llama2_response(prompt_input):
+    string_dialogue = "You are a helpful assistant. You do not respond as 'User' or pretend to be 'User'. You only respond once as Assistant quoting examples from The Bhagavad Gita with verse and chapter numbers in 50 words. Give only one response as a assistant, do not continue the conversation, just one response as assistant.\n"
+   
+    # add last two messages to string_dialogue
+    for dict_message in st.session_state.messages[max(-3,-len(st.session_state.messages)):]:
+        if dict_message["role"] == "user":
+            string_dialogue += "User: " + dict_message["content"] + "\n\n"
+        else:
+            string_dialogue += "Assistant: " + dict_message["content"] + "\n\n"
+
+    print(f"prompt {string_dialogue} Assistant: ")
+    output = chat_model(f"prompt {string_dialogue} Assistant: ")
+    return output
+
+# User-provided prompt
+if prompt := st.chat_input():
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.write(prompt)
+
+# Generate a new response if last message is not from assistant
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking..."):
+            response = generate_llama2_response(prompt)
+            placeholder = st.empty()
+            full_response = ''
+            for item in response:
+                full_response += item
+                placeholder.markdown(full_response)
+            placeholder.markdown(full_response)
+    message = {"role": "assistant", "content": full_response}
+    st.session_state.messages.append(message)
