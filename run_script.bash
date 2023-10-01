5293325be86029bc03348d14526ec74a1788ae1c
Aman Kumar <75082110+Amank2854@users.noreply.github.com>
2023-10-01 21:10:21 +0530

    Update run_script

--- run_script.bash
+++ run_script.bash
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 # Step 1: Start the Docker containers using docker-compose
-docker-compose up -d
+docker run -d -p 8501:8501 amank2854/clockbox:latest
 
-# Step 2: Open the specified URL in a web browser
+# Step 2: Add sleep time to wait for the server to start
+sleep 4
+
+# Step 3: Open the specified URL in a web browser
 # Note: This command will open the URL in the default web browser on Linux.
 # You can adjust it for other operating systems if needed.
 start "http://localhost:8501"
